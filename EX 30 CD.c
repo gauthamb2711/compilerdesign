7b01fdf3cc2835d6993029c2732243b3f59b7737
Gautham <gautham.b2711@gmail.com>
2026-08-23 19:26:17 +0530

    Add files via upload

---
+++ EX 30 CD.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[200];
+    int i, consonants = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        char ch = tolower(str[i]);
+
+        if(ch >= 'a' && ch <= 'z')
+        {
+            if(ch != 'a' && ch != 'e' && ch != 'i' &&
+               ch != 'o' && ch != 'u')
+                consonants++;
+        }
+    }
+
+    printf("Number of consonants = %d\n", consonants);
+
+    return 0;
+}
